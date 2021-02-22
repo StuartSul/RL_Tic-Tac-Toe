bbdd9dbff6b3d47e65c82abf43230a2cc9f688d0
Stuart Sul <ssstuartss@gmail.com>
2021-02-22 21:27:50 +0900

    remove debugging message

--- src/ai/random.py
+++ src/ai/random.py
@@ -16,7 +16,6 @@ class Random:
 
         for i in range(len(board)):
             for j in range(len(board[i])):
-                print('attempting ', i, j, 'num_empty_slots = ',num_empty_slots)
                 if board[i][j] == Board.EMPTY_SLOT:
                     num_empty_slots -= 1
                     if num_empty_slots == next_move:
