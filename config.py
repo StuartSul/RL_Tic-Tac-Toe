67edebd868d4ac5acbcfb7f9e078aa7a3a409289
Stuart Sul <ssstuartss@gmail.com>
2021-02-24 22:21:23 +0900

    set to default

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ class Config:
     SIZE = 3
     SILENT = True
     GUI = True
-    AI_WHITE_ENABLE = False
-    AI_BLACK_ENABLE = True
+    AI_WHITE_ENABLE = True
+    AI_BLACK_ENABLE = False
     AI_WHITE_ALGORITHM = 'reinforcement_learning'
     AI_BLACK_ALGORITHM = 'reinforcement_learning'
     TRAIN_WHITE = False
